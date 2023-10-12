949a38a996322b1d05684ec807a09baf9aaa800b
tinzh <jmz8rm@virginia.edu>
2023-10-12 18:47:29 -0400

    fix

--- race/src/dist_finder.py
+++ race/src/dist_finder.py
@@ -8,7 +8,7 @@ from race.msg import pid_input
 # Some useful variable declarations.
 angle_range = 240	# Hokuyo 4LX has 240 degrees FoV for scan
 desired_distance = 0.9	# distance from the wall (in m). (defaults to right wall). You need to change this for the track
-vel = 15 		# this vel variable is not really used here.
+vel = 15		# this vel variable is not really used here.
 delay = 0.1     # delay in seconds
 forward_projection = vel * delay / 30	# distance (in m) that we project the car forward for correcting the error. You have to adjust this.
 forward_projection = 0.5
@@ -25,10 +25,9 @@ def getRange(data,angle):
     # angle: between -30 to 210 degrees, where 0 degrees is directly to the right, and 90 degrees is directly in front
     # Outputs length in meters to object with angle in lidar scan field of view
     # Make sure to take care of NaNs etc.
-        angle -= math.pi/2
+	angle -= math.pi/2
 	index = (angle - data.angle_min) / data.angle_increment
 	return data.ranges[int(index)]
-funcfuncfunctionfunc
 
 def callback(data):
 	a = getRange(data,theta) # obtain the ray distance for theta
@@ -36,11 +35,11 @@ def callback(data):
 
 	## Your code goes here to determine the projected error as per the alrorithm
 	# Compute Alpha, AB, and CD..and finally the error.
-    	alpha = math.atan((a * math.cos(theta) - b) / (a * math.sin(theta)))
-    	ab = b * math.cos(alpha)
-    	ac = forward_projection
-    	cd = ab + ac * math.sin(alpha)
-    	error = desired_distance - cd
+	alpha = math.atan((a * math.cos(theta) - b) / (a * math.sin(theta)))
+	ab = b * math.cos(alpha)
+	ac = forward_projection
+	cd = ab + ac * math.sin(alpha)
+	error = desired_distance - cd
 
 	msg = pid_input()	# An empty msg is created of the type pid_input
 
@@ -49,13 +48,13 @@ def callback(data):
 	msg.pid_vel = vel		# velocity error can also be sent.
 	pub.publish(msg)
 
-        print("alpha: %lf, ab: %lf" % (alpha, ab))
+	print("alpha: %lf, ab: %lf" % (alpha, ab))
 
 
 if __name__ == '__main__':
 	print("Hokuyo LIDAR node started")
         theta = math.radians(input("theta [50]: ") or 50)
-        desired_distance = input("desired_distance [0.9]: ") or 0.9
+	desired_distance = input("desired_distance [0.9]: ") or 0.9
 
 	rospy.init_node('dist_finder',anonymous = True)
 	# TODO: Make sure you are subscribing to the correct car_x/scan topic on your racecar
