406f2d94b2d61fd00f56ffc62b9e02def9137c88
pnavsingh <pnavsingh@gmail.com>
2023-10-17 01:42:28 -0400

    added changed from libra office

--- race/src/control.py
+++ race/src/control.py
@@ -5,12 +5,25 @@ from race.msg import pid_input
 from ackermann_msgs.msg import AckermannDrive
 
 # PID Control Params
-kp = 0.0 #TODO
-kd = 0.0 #TODO
-ki = 0.0 #TODO
-servo_offset = 0.0	# zero correction offset in case servo is misaligned and has a bias in turning.
+
+servo_offset = 18 # zero correction offset in case servo is misaligned and has a bias in turning.
 prev_error = 0.0
 
+kp_vel = 0.0
+kd_vel = 0.0
+ki_vel = 0.0
+
+min_vel = 15
+
+num_seconds_to_look_at = 1
+hz = 10
+error_array_length = num_seconds_to_look_at * hz
+
+error_array = [0]
+error_sum = 0
+
+last_speed = 0
+last_steering = 0
 
 # This code can input desired velocity from the user.
 # velocity must be between [0,100] to move forward.
@@ -19,51 +32,106 @@ prev_error = 0.0
 # 25: Slow and steady
 # 35: Nice Autonomous Pace
 # > 40: Careful, what you do here. Only use this if your autonomous steering is very reliable.
-vel_input = 0.0	#TODO
+vel_input = 15 #TODO
 
 # Publisher for moving the car.
 # TODO: Use the coorect topic /car_x/offboard/command. The multiplexer listens to this topic
 command_pub = rospy.Publisher('/car_9/offboard/command', AckermannDrive, queue_size = 1)
 
+def bound(val, lower, upper):
+	return min(upper, max(lower, val))
+
 def control(data):
 	global prev_error
 	global vel_input
-	global kp
-	global kd
-	global angle = 0.0
+	kp = 120 #TODO
+	kd = 0#1.5 #TODO
+	ki = 0#12 #TODO
+	global angle
+	global error_sum
+	global error_array
+	global last_speed
+	global last_steering
 
 	print("PID Control Node is Listening to error")
+
+	# An empty AckermannDrive message is created. You will populate the steering_angle and the speed fields.
+	command = AckermannDrive()
 
 	## Your PID code goes here
 	#TODO: Use kp, ki & kd to implement a PID controller
 
 	# 1. Scale the error
 	# 2. Apply the PID equation on error to compute steering
+	error = data.pid_error
 
-	# An empty AckermannDrive message is created. You will populate the steering_angle and the speed fields.
-	command = AckermannDrive()
+	pid_steering_correction = kp * error + kd * (prev_error - error)*10 + ki*(error_sum)
+	angle = pid_steering_correction
+
+	vel_correction = kp_vel * abs(error) + kd_vel * abs(prev_error - error) 
+
+	prev_error = error
+
+	# if last_speed < 1:
+	# error_array = [0]
+	if len(error_array) > error_array_length:
+	error_array = error_array[1:]
+
+	if not math.isnan(error):
+	error_array.append(error)
+
+	error_sum = sum(error_array)*0.1#*len(error_array)
+
+	print("error_sum: %lf" % (error_sum))
 
 	# TODO: Make sure the steering value is within bounds [-100,100]
-	command.steering_angle = angle
+	steering_angle = angle + servo_offset
+	# if math.isnan(steering_angle):
+	# steering_angle = 
+	steering_angle = bound(steering_angle, -100, 100)
+	command.steering_angle = steering_angle
 
 	# TODO: Make sure the velocity is within bounds [0,100]
-	command.speed = vel_input
+	# max possible velocity is pid_vel, vel_correction decreases vel
+	pid_vel = bound(data.pid_vel, 0, 100)
+	vel = pid_vel - vel_correction
+	vel = bound(vel, min_vel, pid_vel)
+	command.speed = vel
+	vel_coefficient = 25/(abs(steering_angle)+1)
+
+	command.speed = min(max(min_vel, vel_coefficient * data.pid_vel), data.pid_vel)
+	#vel_correction = 0
+	if(abs(command.steering_angle) < 50):
+	command.speed = data.pid_vel
+	else:
+	command.speed = min_vel
+
+	print("angle: %lf", angle)
+
+	print("steering_angle: %lf, vel_coefficient: %lf, speed: %lf, pid_vel: %lf, vel_correction: %lf" % (command.steering_angle, vel_coefficient, command.speed, data.pid_vel, vel_correction))
+	last_speed = command.speed
+	last_steering = command.steering_angle
 
 	# Move the car autonomously
 	command_pub.publish(command)
 
 if __name__ == '__main__':
 
-    # This code tempalte asks for the values for the gains from the user upon start, but you are free to set them as ROS parameters as well.
+	# This code tempalte asks for the values for the gains from the user upon start, but you are free to set them as ROS parameters as well.
 	global kp
 	global kd
 	global ki
 	global vel_input
-	kp = input("Enter Kp Value: ")
-	kd = input("Enter Kd Value: ")
-	ki = input("Enter Ki Value: ")
-	vel_input = input("Enter desired velocity: ")
+	# kp = float(raw_input("Enter Kp Value [250]: ") or "250")
+	# kd = float(raw_input("Enter Kd Value [0]: ") or "0")
+	# ki = float(raw_input("Enter Ki Value [0]: ") or "0")
+
+	# kp_vel = float(raw_input("Enter vel Kp Value [0]: ") or "0")
+	# kd_vel = float(raw_input("Enter vel Kd Value [0]: ") or "0")
+	# ki_vel = float(raw_input("Enter vel Ki Value [0]: ") or "0")
+	# vel_input = int(input("Enter desired velocity [15]: ") or "15")
 	rospy.init_node('pid_controller', anonymous=True)
-    # subscribe to the error topic
+	# subscribe to the error topic
 	rospy.Subscriber("error", pid_input, control)
 	rospy.spin()
+
--- race/src/dist_finder.py
+++ race/src/dist_finder.py
@@ -6,49 +6,59 @@ from sensor_msgs.msg import LaserScan
 from race.msg import pid_input
 
 # Some useful variable declarations.
-angle_range = 240	# Hokuyo 4LX has 240 degrees FoV for scan
-forward_projection = 1.5	# distance (in m) that we project the car forward for correcting the error. You have to adjust this.
-desired_distance = 0.9	# distance from the wall (in m). (defaults to right wall). You need to change this for the track
-vel = 15 		# this vel variable is not really used here.
-error = 0.0		# initialize the error
+angle_range = 240 # Hokuyo 4LX has 240 degrees FoV for scan
+desired_distance = 0.8 # distance from the wall (in m). (defaults to right wall). You need to change this for the track
+vel = 30 # this vel variable is not really used here.
+delay = 0.1 # delay in seconds
+forward_projection = vel * delay / 30 # distance (in m) that we project the car forward for correcting the error. You have to adjust this.
+forward_projection = 1
+error = 0.0 # initialize the error
 car_length = 0.50 # Traxxas Rally is 20 inches or 0.5 meters. Useful variable.
+theta = math.radians(70) # angle being swept (in degrees)
 
 # Handle to the publisher that will publish on the error topic, messages of the type 'pid_input'
 pub = rospy.Publisher('error', pid_input, queue_size=10)
 
-
 def getRange(data,angle):
 	# data: single message from topic /scan
-    # angle: between -30 to 210 degrees, where 0 degrees is directly to the right, and 90 degrees is directly in front
-    # Outputs length in meters to object with angle in lidar scan field of view
-    # Make sure to take care of NaNs etc.
-    #TODO: implement
-	return 0.0
-
-
+	# angle: between -30 to 210 degrees, where 0 degrees is directly to the right, and 90 degrees is directly in front
+	# Outputs length in meters to object with angle in lidar scan field of view
+	# Make sure to take care of NaNs etc.
+	angle -= math.pi/2
+	index = (angle - data.angle_min) / data.angle_increment
+	if math.isnan(data.ranges[int(index)]):
+	return 5
+	return data.ranges[int(index)]
 
 def callback(data):
-	global forward_projection
-
-	theta = 50 # you need to try different values for theta
 	a = getRange(data,theta) # obtain the ray distance for theta
-	b = getRange(data,0)	# obtain the ray distance for 0 degrees (i.e. directly to the right of the car)
-	swing = math.radians(theta)
+	b = getRange(data,0) # obtain the ray distance for 0 degrees (i.e. directly to the right of the car)
 
 	## Your code goes here to determine the projected error as per the alrorithm
 	# Compute Alpha, AB, and CD..and finally the error.
-	# TODO: implement
+	alpha = math.atan((a * math.cos(theta) - b) / (a * math.sin(theta)))
+	ab = b * math.cos(alpha)
+	ac = forward_projection
+	cd = ab + ac * math.sin(alpha)
+	error = desired_distance - cd
 
-	msg = pid_input()	# An empty msg is created of the type pid_input
+	msg = pid_input() # An empty msg is created of the type pid_input
+
 	# this is the error that you want to send to the PID for steering correction.
 	msg.pid_error = error
-	msg.pid_vel = vel		# velocity error can also be sent.
+	msg.pid_vel = vel # velocity error can also be sent.
 	pub.publish(msg)
 
+	print("alpha: %lf, ab: %lf" % (alpha, ab))
 
 if __name__ == '__main__':
 	print("Hokuyo LIDAR node started")
+	# theta = math.radians(float(raw_input("theta [50]: ") or "50"))
+	# desired_distance = float(raw_input("desired_distance [0.9]: ") or "0.9")
+	#vel = float(raw_input("velocity [15]: ") or "15")
+
 	rospy.init_node('dist_finder',anonymous = True)
 	# TODO: Make sure you are subscribing to the correct car_x/scan topic on your racecar
-	rospy.Subscriber("/car_X/scan",LaserScan,callback)
+	rospy.Subscriber("/car_9/scan",LaserScan,callback)
 	rospy.spin()
+
