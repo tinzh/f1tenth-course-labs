a3329d22fa2106e6661629fc93604b656a7fe212
tinzh <jmz8rm@virginia.edu>
2023-10-29 13:45:18 -0400

    fix

--- race/src/gap_finder.py
+++ race/src/gap_finder.py
@@ -65,7 +65,7 @@ def callback(data):
 		print(closest_distance)
 		print(params["car_width"] / 4 / closest_distance)
 		index_width = 2 * math.asin(params["car_width"] / (4.0 * closest_distance))
-		index_width = math.ceil(index_width)
+		index_width = int(math.ceil(index_width))
 
 		for i in range(disparity[0], 
 				       disparity[0] + direction_sign * index_width, 
