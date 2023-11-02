6e82cb8b85424317f3af22eaac5b0ebba7f05b4c
Cole Blackman <coleblax@gmail.com>
2023-11-02 13:01:08 -0400

    Added RVIZ red gap: publishing to topic

--- race/src/gap_finder.py
+++ race/src/gap_finder.py
@@ -107,3 +107,25 @@ if __name__ == "__main__":
 	rospy.init_node('gap_finder',anonymous = True)
 	rospy.Subscriber("/car_9/scan",LaserScan,callback)
 	rospy.spin()
+
+	# RVIZ, red dots representing the gap detection, uses laserscan message type but isn't actually a laserscan.
+
+	#Not sure about these definitions
+	gap_start_index = deepest_gap - index_width
+    	gap_end_index = deepest_gap + index_width
+	gap_start_index = max(0, gap_start_index)
+	gap_end_index = min(len(data.ranges) - 1, gap_end_index)
+	#Satisfy laserscan
+	coles_gap = LaserScan()
+    	coles_gap.header = data.header
+    	coles_gap.angle_min = index_to_angle(gap_start_index)
+    	coles_gap.angle_max = index_to_angle(gap_end_index)
+    	coles_gap.ranges = [data.ranges[i] for i in range(gap_start_index, gap_end_index + 1)]
+    	coles_gap.angle_increment = data.angle_increment
+    	coles_gap.time_increment = data.time_increment
+    	coles_gap.scan_time = data.scan_time
+    	coles_gap.range_min = data.range_min
+    	coles_gap.range_max = data.range_max
+
+	pub.publish(coles_gap)
+
